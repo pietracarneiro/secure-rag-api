f38f109078d5c1a8dea11bb1af564d6f3d735536
Pietra Carneiro <pietrasc98@gmail.com>
2026-07-14 22:39:08 -0300

    fix(config): centralizando DATABASE_URL no .env e ajuste Alembic

--- alembic/env.py
+++ alembic/env.py
@@ -1,5 +1,6 @@
 from logging.config import fileConfig
 
+from app.core.config import settings
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
@@ -11,6 +12,11 @@ from app.modules.auth.models import User
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+config.set_main_option(
+    "sqlalchemy.url",
+    settings.database_url.replace("%", "%%"),
+)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
--- app/core/database.py
+++ app/core/database.py
@@ -1,21 +1,23 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-# Endereço de conexão: postgres://usuario:senha@localhost:porta/nome_do_banco
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:mysecretpassword@localhost:5432/secureragdb"
+from app.core.config import settings
 
-# O engine é o motor que gerencia a conexão física com o banco
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-# Cada objeto SessionLocal será uma conversa aberta com o banco de dados
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(settings.database_url)
 
-# Essa classe Base será herdada por todas as tabelas que criarmos no futuro
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+)
+
 Base = declarative_base()
 
-# Função auxiliar para abrir e fechar a conexão com o banco automaticamente
+
 def get_db():
     db = SessionLocal()
+
     try:
         yield db
     finally:
